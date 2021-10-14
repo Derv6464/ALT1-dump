56a8daec60abcf2ef894063471cba742fbd3f70d
Dervla <36761419+Derv6464@users.noreply.github.com>
2021-10-14 09:54:40 +0100

    Add files via upload

--- client.js
+++ client.js
@@ -6,33 +6,40 @@ console.log("hello world :o");
 const dreams = [];
 
 // define variables that reference elements on our page
-const dreamsForm = document.forms[0];
-const dreamInput = dreamsForm.elements["dream"];
-const dreamsList = document.getElementById("dreams");
-const clearButton = document.querySelector('#clear-dreams');
+const comicForm = document.forms[0];
+const searchInput = comicForm.elements["character"];
+const dreamsList = document.getElementById("add-char");
+const clearButton = document.querySelector('#clear-char');
 
 // request the dreams from our app's sqlite database
-fetch("/getDreams", {})
-  .then(res => res.json())
-  .then(response => {
-    response.forEach(row => {
-      appendNewDream(row.dream);
-    });
-  });
+//fetch("/getDreams", {})
+//  .then(res => res.json())
+//  .then(response => {
+//   response.forEach(row => {
+//      appendNewDream(row.dream);
+//    });
+//  });
 
 // a helper function that creates a list item for a given dream
-const appendNewDream = dream => {
-  const newListItem = document.createElement("li");
-  newListItem.innerText = dream;
-  dreamsList.appendChild(newListItem);
-};
+//const appendNewDream = dream => {
+//  const newListItem = document.createElement("li");
+//  newListItem.innerText = dream;
+//  dreamsList.appendChild(newListItem);
+//};
 
 // listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = event => {
+function submitChar(){
   // stop our form submission from refreshing the page
   event.preventDefault();
+  let searchKey = searchInput.value;
+  console.log(searchKey);
+  document.getElementById("srchChar").innerHTML = searchKey;
+};
 
-  const data = { dream: dreamInput.value };
+function clearChar(){
+  pass
+}
+/*  const data = { dream: dreamInput.value };
 
   fetch("/addDream", {
     method: "POST",
@@ -51,12 +58,15 @@ dreamsForm.onsubmit = event => {
   dreamInput.value = "";
   dreamInput.focus();
 };
+*/
 
+/*
 clearButton.addEventListener('click', event => {
   fetch("/clearDreams", {})
     .then(res => res.json())
     .then(response => {
-      console.log("cleared dreams");
+      console.log("cleared character");
     });
   dreamsList.innerHTML = "";
 });
+*/
